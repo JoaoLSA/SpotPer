43cd05eff19c153bcb84fc68d186242e84fadf02
joaolsa <joaoluizdoss@gmail.com>
2019-11-10 18:01:26 -0300

    Fix bug and edit env.py.example

--- app/main.py
+++ app/main.py
@@ -1,5 +1,4 @@
 from resources import app
-import settings
 # Some other example server values are
 # server = 'localhost\sqlexpress' # for a named instance
 # server = 'myserver,port' # to specify an alternate port
