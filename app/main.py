9159eecaba7863ca54ac6d9bceac9bc9729aaf63
413514@lec.dc.ufc.br <413514@lec.dc.ufc.br>
2019-11-11 16:24:40 -0300

    Remove .pyc

--- app/main.py
+++ app/main.py
@@ -1,8 +1,4 @@
 from resources import app
-# Some other example server values are
-# server = 'localhost\sqlexpress' # for a named instance
-# server = 'myserver,port' # to specify an alternate port
-
 
 if __name__ == '__main__':
     app.run(debug=True)
