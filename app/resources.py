06049ee7d1c1655d6f1b6567c0f9b7d55f430450
joaolsa <joaoluizdoss@gmail.com>
2019-11-30 13:48:37 -0300

    Playlist songs

--- app/resources.py
+++ app/resources.py
@@ -8,9 +8,10 @@ cnxn = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER='+SERVER+';
 cursor = cnxn.cursor()
 
 # resource to query all playlists
+@app.route("/", methods=["GET"])
 @app.route("/playlist/", methods=["GET"])
 def get_playlist():
-     cursor.execute("SELECT * FROM filiais")
+     cursor.execute("SELECT * FROM playlist")
      columns = [column[0] for column in cursor.description]
      playlists = []
      for row in cursor.fetchall():
@@ -21,6 +22,37 @@ def get_playlist():
 @app.route("/playlist/<int:cod>", methods=["GET"])
 def get_songs(cod = None):
      # Show playlist songs to the user
-     return render_template("playlist/index.html",
-     playlists=playlists)
-@app.route("/playlist/<", methods=["POST"])
+     cursor.execute("""select F.descricao from
+     Faixa f, playlist p, PlaylistFaixa PF
+     where
+          f.cod = PF.cod_faixa and
+          PF.cod_playlist = {} and
+          p.cod = {}""".format(str(cod), str(cod))
+     )
+     columns = [column[0] for column in cursor.description]
+     songs = []
+     for row in cursor.fetchall():
+          songs.append(dict(zip(columns, row)))
+     return render_template("playlist/songs/index.html",
+     content= {
+               "playlist_cod": cod,
+               "songs": songs
+          }
+     )
+@app.route("/playlist/<int:cod>/add", methods=["GET"])
+def add_songs(cod_playlist =None):
+     cursor.execute("""
+          select F.cod, F.descricao
+          from
+               Faixa F
+          where
+               f.cod = PF.cod_faixa and
+               PF.cod_playlist <> {} and
+               p.cod = {}""".format(str(cod_playlist), str(cod_playlist))
+     )
+     columns = [column[0] for column in cursor.description]
+     songs = []
+     for row in cursor.fetchall():
+          songs.append(dict(zip(columns, row)))
+     return render_template("playlist/songs/add/index.html",
+     songs=songs)
